4580ea6265a02e90576688c2162445e45bd0052a
Nathanael Carauna <nathanael.carauna@gmail.com>
2022-04-07 22:31:29 -0300

    Custom validation

--- AbbyWeb/Model/Category.cs
+++ AbbyWeb/Model/Category.cs
@@ -9,7 +9,7 @@ namespace AbbyWeb.Model
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        [DisplayName("Display Order")]
+        [Display(Name ="Display Order")]
         [Range(1,100, ErrorMessage = "The Display order field must be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
