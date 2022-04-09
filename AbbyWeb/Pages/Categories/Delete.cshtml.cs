964473df46fc0fc659e4ed47e0de1426e2cea2cf
Nathanael Carauna <nathanael.carauna@gmail.com>
2022-04-09 19:56:50 -0300

    Delete Category implemented

---
+++ AbbyWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,33 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+
+            _db.Category.Remove(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+
+        }
+    }
+}
